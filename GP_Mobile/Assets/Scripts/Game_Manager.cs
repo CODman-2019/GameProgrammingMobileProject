e4ed8de56824382fc7d820fb6930f9caeff25207
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-10-21 08:58:32 -0300

    cahracter controller + gameManager

---
+++ GP_Mobile/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager manager;
+
+    public GameObject player;
+    //public Scene current;
+
+
+
+
+    void Awake()
+    {
+        if(manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            manager = this;
+        }
+        else if(manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+class Data
+{
+    public int level;
+    public GameObject player;
+}
---
+++ GP_Mobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera view;
+    public NavMeshAgent agent;
+
+    public float distance;
+    public bool 
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the left 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = view.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(Vector3.Distance(hit.point, transform.position) < distance)                // move the player
+                agent.SetDestination(hit.point);
+
+            }
+
+
+        }
+    }
+}
